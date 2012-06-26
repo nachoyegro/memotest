462568d0a02ddbf9b2ff2b7a0b0c9b9490f97180
Juan Ignacio Yegro <nacho.acade@hotmail.com>
2012-06-26 15:01:01 -0300

    Bienvenido y crear partida

--- application.rb
+++ application.rb
@@ -1,9 +1,13 @@
 require 'sinatra/base'
 
 class MyApplication < Sinatra::Base
-  get '/' do
-    
-  end
 
+	get '/' do
+		erb :bienvenido
+	end
+
+	post '/crear_partida' do
+		erb :crear_partida
+	end
 
 end
